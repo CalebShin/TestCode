432fa2f460c81f537501efbc02f495ea7cf8ce48
CalebShin <senovoa@naver.com>
2018-06-27 00:00:48 +0900

    함수이름 오류 수정

--- TestCode/Report01.py
+++ TestCode/Report01.py
@@ -4,11 +4,9 @@ def parentheses_repression(text):
     return bool(re.search('^\(([\(\)]{0,99998})\)$',text));
 
 if __name__ == "__main__":
-    print(parentheses("()()"),"\n")
-    print(parentheses("(())()"),"\n")
-    print(parentheses(")()("),"\n")
-    print(parentheses("(()("),"\n")
-
-
+    print(parentheses_repression(")()()"),"\n")
+    print(parentheses_repression("(()))))))(((()()"),"\n")
+    print(parentheses_repression(")()))))))()("),"\n")
+    print(parentheses_repression(")))))))))))))"),"\n")
 
     
